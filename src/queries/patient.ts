74292f55cf746461d6107dff3172d2dbb75fe768
navi-prem <naviprem.2005@gmail.com>
2024-03-16 17:03:49 +0530

    👻 FEAT: JOIN query added.

--- src/queries/patient.ts
+++ src/queries/patient.ts
@@ -1,3 +1,3 @@
-const getPatient = "select * from patient JOIN record ON patient.email = record.email where email = $1;"
+const getPatient = "select * from patient JOIN record ON patient.email = record.email where record.email = $1;"
 
 export default { getPatient }
