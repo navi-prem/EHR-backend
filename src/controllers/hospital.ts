4ae626d82b3f9d9063b87e4c67ba92159c35f937
navi-prem <naviprem.2005@gmail.com>
2024-03-16 19:09:50 +0530

    👻 FEAT: Doctors and Hospitals details joined.

--- src/controllers/hospital.ts
+++ src/controllers/hospital.ts
@@ -2,14 +2,17 @@ import { Request, Response } from "express"
 import { pool } from "../../db"
 import { Hospital } from "../queries"
 
-export const getDoctors = async (req: Request, res: Response) => {
+export const getDetails = async (req: Request, res: Response) => {
     const { hospital_id } = req.body
     if (!hospital_id) return res.status(417).send("Unexpected params.")
 
     const client = await pool.connect()
 
     try {
-        const { rows } = await client.query(Hospital.getDoctors, [hospital_id])
+        const { rows } = await client.query(Hospital.getDetails, [hospital_id])
+        const { rows: doctors } = await client.query(Hospital.getDoctors, [hospital_id])
+        doctors.forEach(i => delete i.pass)
+        rows[0].doctors = doctors
         client.release()
         return res.status(200).json(rows)
     } catch (err) {
@@ -76,22 +79,6 @@ export const deleteDoctor = async (req: Request, res: Response) => {
         const { rows } = await client.query(Hospital.deleteDoctor, [doctor_id])
         client.release()
         return res.status(200).send("Doctor deleted successfully.")
-    } catch (err) {
-        client.release()
-        return res.status(400).send("Internal Server Error.")
-    }
-}
-
-export const getDetails = async (req: Request, res: Response) => {
-    const { hospital_id } = req.body
-    if (!hospital_id) return res.status(417).send("Unexpected params.")
-
-    const client = await pool.connect()
-
-    try {
-        const { rows } = await client.query(Hospital.getDetails, [hospital_id])
-        client.release()
-        return res.status(200).json(rows)
     } catch (err) {
         client.release()
         return res.status(400).send("Internal Server Error.")
--- src/routes/hospital.ts
+++ src/routes/hospital.ts
@@ -1,11 +1,10 @@
 import express, { Router } from "express"
 import { verifyHospitalToken } from "../middlewares"
-import { getDoctors, addDoctor, updateDoctor, deleteDoctor, getDetails } from "../controllers"
+import { addDoctor, updateDoctor, deleteDoctor, getDetails } from "../controllers"
 
 const BASE_ROUTE: string = '/hospital'
 const router: Router = express.Router()
 
-router.post('/get-doctors', verifyHospitalToken, getDoctors)
 router.post('/add-doctor', verifyHospitalToken, addDoctor)
 router.put('/update-doctor', verifyHospitalToken, updateDoctor)
 router.delete('/delete-doctor', verifyHospitalToken, deleteDoctor)
