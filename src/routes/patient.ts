3ae3689fa5342fa8a573582d2fa30027936bf81e
navi-prem <naviprem.2005@gmail.com>
2024-03-17 13:39:11 +0530

    👻 FEAT: proxy route added. #2

--- src/routes/patient.ts
+++ src/routes/patient.ts
@@ -1,11 +1,12 @@
 import express, { Router } from "express"
 import { verifyToken } from "../middlewares"
-import { getPatient, signUp } from "../controllers"
+import { getPatient, proxy, signUp } from "../controllers"
 
 const BASE_ROUTE: string = '/patient'
 const router: Router = express.Router()
 
 router.post('/get-patient', verifyToken, getPatient)
 router.post('/sign-up', signUp)
+router.post('/proxy', proxy)
 
 export default { BASE_ROUTE, router }
